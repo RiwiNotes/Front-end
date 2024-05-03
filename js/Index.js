de9f9a5eba12f6025f261dad2b826791203de6df
DeklanBeckett <juandausuga16@gmail.com>
2024-05-03 13:21:01 -0500

    solo falta editar y buscar y cuadrar mas front

--- js/Index.js
+++ js/Index.js
@@ -1,14 +1,20 @@
 let notes = document.getElementById("notes");
 fetch("http://localhost:5024/api/Notas").then(r => r.json())
 .then(data =>{
- data.forEach(element => {
+data.reverse();
+data.forEach(element => {
     notes.innerHTML += `
+    <div class="col-md-3">
+    <div class="notes-container">
         <div class="note">
             <h2>${element.titulo}</h2>
-            <p>${element.contenido}</p>
+            <p class="truncate-text">${element.contenido}</p>
             <p>${element.fecha}</p>
-            <button class="btn btn-info" onclick="verDetalles(${element.id})">Ver Detalles</button>
+            <button class="buttonn" onclick="verDetalles(${element.id})">Ver Nota</button>
         </div>
+    </div>
+</div>
+
         `
     });
 })
@@ -16,11 +22,11 @@ fetch("http://localhost:5024/api/Notas").then(r => r.json())
 
 // fetch categorias 
 let select = document.getElementById("select");
-fetch("http://localhost:5024/api/Categorias").then(response => response.json())
+fetch("http://localhost:5024/api/Notas").then(response => response.json())
 .then(data => {
     data.forEach(categoria => {
     select.innerHTML += `
-    <option value="${categoria.id}">${categoria.nombre_categoria}</option>
+    <option value="${categoria.categoria}">${categoria.categoria    }</option>
     
 `
     
@@ -32,7 +38,7 @@ fetch("http://localhost:5024/api/Categorias").then(response => response.json())
 .then(data => {
     data.forEach(categoria => {
     select2.innerHTML += `
-    <option value="${categoria.id}">${categoria.nombre_categoria}</option>
+    <option value="${categoria.nombre_categoria}">${categoria.nombre_categoria}</option>
     
 `
     
@@ -50,7 +56,7 @@ function verDetalles(id){
             <p>${data.contenido}</p>
             <p>${data.fecha}</p>
             <div class="misbotones">
-            <a href="" class="btn btn-info"">Volver</a>
+            <a href="" class="buttonn"">Volver</a>
             <button class="delete-button" type="submit"  onclick="EliminarNota(${data.id})">
                 <svg class="delete-svgIcon" viewBox="0 0 448 512">
                                 <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
@@ -63,26 +69,27 @@ function verDetalles(id){
 }
 
 function EliminarNota(id) {
-    fetch(`http://localhost:5024/api/Notas/${id}` ,{method : "DELETE",
-        headers:{
+    fetch(`http://localhost:5024/api/Notas/${id}`, {
+        method: "DELETE",
+        headers: {
             "Content-Type": "application/json"
-        }})
-        .then(r => r.json())
-        .then(data => {
-            
-            alert("Se ha eliminado una nota");
-            
-            location.href="";
-        });
+        }
+    })
+    .then(r => r.json())
+    .then(data => {
+        alert("Se ha eliminado una nota");
+        location.reload();  
+    });
 }
 
 function crearNota(){
     let titulo = document.getElementById("titulo").value;
     let contenido = document.getElementById("contenido").value;
-    let fecha = "2024-04-30"
+    let fecha = new Date(Date.now()); 
     let categoria = document.getElementById("select2").value;
-    fetch("http://localhost:5024/api/Notas",{method : "POST",
-        headers:{
+    fetch("http://localhost:5024/api/Notas",{
+        method: "POST",
+        headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -91,5 +98,10 @@ function crearNota(){
             fecha,
             categoria
         })
-    }).then(r => r.json())
+    }).then(r => {
+        if(r.ok){
+            location.reload(); 
+        }
+        return r.json();
+    });
 }
