3a0583039fe4b867403324d5bf66e7141c80c3cf
DeklanBeckett <juandausuga16@gmail.com>
2024-05-03 15:50:49 -0500

    cuadré más estilos

--- js/Index.js
+++ js/Index.js
@@ -5,7 +5,7 @@ data.reverse();
 data.forEach(element => {
     notes.innerHTML += `
     <div class="col-md-3">
-    <div class="notes-container">
+    <div class="notes2-container">
         <div class="note">
             <h2>${element.titulo}</h2>
             <p class="truncate-text">${element.contenido}</p>
@@ -22,11 +22,11 @@ data.forEach(element => {
 
 // fetch categorias 
 let select = document.getElementById("select");
-fetch("http://localhost:5024/api/Notas").then(response => response.json())
+fetch("http://localhost:5024/api/Categorias").then(response => response.json())
 .then(data => {
     data.forEach(categoria => {
     select.innerHTML += `
-    <option value="${categoria.categoria}">${categoria.categoria    }</option>
+    <option value="${categoria.nombre_categoria}">${categoria.nombre_categoria   }</option>
     
 `
     
@@ -51,18 +51,24 @@ fetch("http://localhost:5024/api/Categorias").then(response => response.json())
 function verDetalles(id){
     fetch(`http://localhost:5024/api/Notas/${id}`).then(r => r.json()).then(data => {
     notes.innerHTML = `
-    <div class="note">
-            <h2>${data.titulo}</h2>
-            <p>${data.contenido}</p>
-            <p>${data.fecha}</p>
-            <div class="misbotones">
-            <a href="" class="buttonn"">Volver</a>
-            <button class="delete-button" type="submit"  onclick="EliminarNota(${data.id})">
-                <svg class="delete-svgIcon" viewBox="0 0 448 512">
-                                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
-                                </svg>
-            </button>
-        </div>
+    <div class="col-md-12">
+                <div class="notes-container">
+                    <div class="note2">
+                        <h2>${data.titulo}</h2>
+                        <p>${data.contenido}</p>
+                        <p>${data.fecha}</p>
+                        <div class="misbotones">
+                        <a href="" class="buttonn"">Volver</a>
+                        <button class="delete-button" type="submit"  onclick="EliminarNota(${data.id})">
+                            <svg class="delete-svgIcon" viewBox="0 0 448 512">
+                                            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+            </div>
     `
 })
     
